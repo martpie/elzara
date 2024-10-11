70c4954d075f70410254dc4abdfaa1b54011c499
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-10-11 20:01:03 +0200

    temp

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig, squooshImageService } from "astro/config";
-
-// https://astro.build/config
-export default defineConfig({
-  image: {
-    service: squooshImageService(),
-  },
-});
