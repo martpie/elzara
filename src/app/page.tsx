4dcc4bbee9baa54fa165c23c953fbbe81110241b
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-10-13 12:48:04 +0200

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-	return <div>Hello World!</div>;
+import { getDocuments } from "outstatic/server";
+
+export default async function Home() {
+	const events = await getData();
+	return (
+		<div>
+			Hellow World
+			<pre>{JSON.stringify(events, null, 2)}</pre>
+			{events.map((post) => (
+				<h1 key={post.slug}>{post.title}</h1>
+			))}
+		</div>
+	);
+}
+
+async function getData() {
+	const events = getDocuments("events", ["title"]);
+
+	return events;
 }
