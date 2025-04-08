8fe9eb10fa9fa6c5b35fc78d0d829866708c6310
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-04-08 21:59:57 +0200

    Responsive improvements + sticky header

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
       url: "/favicon.png",
       type: "image/png",
       sizes: "96x96",
-    }
-  ]
+    },
+  ],
 };
 
 export default function Layout({
@@ -31,27 +31,25 @@ export default function Layout({
     <html lang="en">
       <body className={`${nanum.className} ${styles.layout}`}>
         <NavigationProgress />
-        <header>
-          <div className={styles.header}>
-            <div className={styles.headerSection}>
-              <Link href="/">Home</Link>
-              <br />
-              <Link href="/portfolio.pdf" target="_blank" rel="noreferrer">
-                Portfolio
-              </Link>
-            </div>
-            <div className={styles.headerSection}>
-              <Link
-                href="https://www.instagram.com/elzara_oiseau/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                @elzara_oiseau
-              </Link>
-              <br />
-              {/* obfuscated-kinda email, not sure it has any value */}
-              &#101;&#108;&#122;&#097;&#114;&#097;&#111;&#105;&#115;&#101;&#097;&#117;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;
-            </div>
+        <header className={styles.header}>
+          <div className={styles.headerSection}>
+            <Link href="/">Home</Link>
+            <br />
+            <Link href="/portfolio.pdf" target="_blank" rel="noreferrer">
+              Portfolio
+            </Link>
+          </div>
+          <div className={styles.headerSection}>
+            <Link
+              href="https://www.instagram.com/elzara_oiseau/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @elzara_oiseau
+            </Link>
+            <br />
+            {/* obfuscated-kinda email, not sure it has any value */}
+            &#101;&#108;&#122;&#097;&#114;&#097;&#111;&#105;&#115;&#101;&#097;&#117;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;
           </div>
         </header>
         <main className={styles.main}>{children}</main>
