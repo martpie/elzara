9f079f9c6cfd7da4ad831166470d76b64867c9bb
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-04-10 17:18:54 +0200

    Openable images in Markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,4 +1,4 @@
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { type Components } from "react-markdown";
 import remarkYoutube from "remark-youtube";
 
 import styles from "./Markdown.module.css";
@@ -7,12 +7,52 @@ type Props = {
   content: string;
 };
 
-const PLUGINS = [remarkYoutube];
+/**
+ * Custom <img /> to see the image in full screen
+ */
+const MarkdownImage: Components["img"] = ({ node, ...props }) => {
+  return (
+    <a href={props.src} target="_blank" rel="noreferrer" className={styles.img}>
+      <img {...props} />
+    </a>
+  );
+};
 
+/**
+ * Custom paragraph, unwrapping <p> with a single <img /> in them
+ */
+const MarkdownParagraph: Components["p"] = ({ node, children, ...props }) => {
+  if (!children) {
+    return null;
+  }
+
+  // If the paragraph only contains an image, unwrap it
+  if (
+    typeof children === "object" &&
+    "type" in children &&
+    children.type === "img"
+  ) {
+    return children;
+  }
+
+  return <p {...props}>{children}</p>;
+};
+
+const PLUGINS = [remarkYoutube];
+const COMPONENTS: Components = {
+  img: MarkdownImage,
+  p: MarkdownParagraph,
+};
+
+/**
+ * Markdown, but spicy
+ */
 export default function Markdown(props: Props) {
   return (
     <div className={styles.markdown}>
-      <ReactMarkdown remarkPlugins={PLUGINS}>{props.content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={PLUGINS} components={COMPONENTS}>
+        {props.content}
+      </ReactMarkdown>
     </div>
   );
 }
