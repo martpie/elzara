f15c5d41a1c3c19b460a53feefb4f286fc4c2c19
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-04-10 15:50:59 +0200

    Re-introduce remark-youtube

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,14 +1,18 @@
 import ReactMarkdown from "react-markdown";
+import remarkYoutube from "remark-youtube";
+
 import styles from "./Markdown.module.css";
 
 type Props = {
   content: string;
 };
 
+const PLUGINS = [remarkYoutube];
+
 export default function Markdown(props: Props) {
   return (
     <div className={styles.markdown}>
-      <ReactMarkdown>{props.content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={PLUGINS}>{props.content}</ReactMarkdown>
     </div>
   );
 }
